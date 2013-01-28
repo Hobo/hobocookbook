e1326ca416250b99ab044a73bb9347f6e8ce85a7
Bryan Larsen <bryan@larsen.st>
2013-01-28 10:16:41 -0500

    fix a common 404 when switching versions.

--- app/controllers/api_taglibs_controller.rb
+++ app/controllers/api_taglibs_controller.rb
@@ -6,4 +6,11 @@ class ApiTaglibsController < ApplicationController
 
   auto_actions :show
 
+  def show
+    if params[:id] =~ /^rapid_(.*)/
+      params[:id] = $1
+    end
+    hobo_show
+  end
+
 end
